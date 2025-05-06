60fb36e05034e1910d99446008bb7066c71060f5
mikeingood <mikeingood@ya.ru>
2025-05-06 11:35:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const words = [
-  "огонь", "сцена", "поток", "харизма", "сила",
-  "деньги", "уверенность", "взлет", "ритм", "кач"
+let words = [];
 
-];
+fetch('words.txt')
+.then(response => response.text())
+.then(text => {
+  words = text.split ('\n').map(w => w.trim()).filter(boolean);
+});
+
 function getRandomWord() {
   const index = Math.floor(Math.random() * words.length);
   document.getElementById('word').textContent = words[index];
